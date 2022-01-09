5843a62b6f6e39ed6bdb9cb7101807a2d33f3486
Maitra Khatri <maitrakhatri@gmail.com>
2022-01-09 19:09:25 +0530

    updated code to ES6+

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var billAmount = document.querySelector("#bill-amount")
-var cashGiven = document.querySelector("#cash-given")
-var checkButton = document.querySelector("#check-button")
-var errorMessage = document.querySelector("#error-message")
-var returnAmount = document.querySelector("#reutrn-amount")
-var numberofnotes = document.querySelectorAll(".no-of-notes")
-var cashGivenSection = document.querySelector(".cashgiven")
+const billAmount = document.querySelector("#bill-amount")
+const cashGiven = document.querySelector("#cash-given")
+const checkButton = document.querySelector("#check-button")
+const errorMessage = document.querySelector("#error-message")
+const returnAmount = document.querySelector("#reutrn-amount")
+const numberofnotes = document.querySelectorAll(".no-of-notes")
+const cashGivenSection = document.querySelector(".cashgiven")
 
-var dominationAvailable = [2000, 500, 100, 20, 10, 5, 1]
+const dominationAvailable = [2000, 500, 100, 20, 10, 5, 1]
 
 cashGivenSection.style.display = "none";
 
-billAmount.addEventListener("change", function changeHandler() {
-    cashGivenSection.style.display = "block";
-})
+billAmount.addEventListener("change", () => cashGivenSection.style.display = "block")
 
-checkButton.addEventListener("click", function onClickHandler() {
-    
+// billAmount.addEventListener("change", function changeHandler() {
+//     cashGivenSection.style.display = "block";
+// })
+
+checkButton.addEventListener("click", () => {
+
     errorMessage.style.display = "none";
     if(billAmount.value > 0) {
 
@@ -39,14 +41,18 @@ checkButton.addEventListener("click", function onClickHandler() {
     }
 })
 
-function showMessage(message) {
+const showMessage = message => {
     errorMessage.style.display = "block";
     errorMessage.innerText = message;
 }
 
+// function showMessage(message) {
+//     errorMessage.style.display = "block";
+//     errorMessage.innerText = message;
+// }
 
-function noOfNotesToReturn(amountLeft) {
 
+const noOfNotesToReturn = amountLeft => {
     for(i=0; i < dominationAvailable.length; i++) {
 
         const noofnotes = Math.trunc(amountLeft/dominationAvailable[i]);
@@ -54,7 +60,17 @@ function noOfNotesToReturn(amountLeft) {
         numberofnotes[i].innerText = noofnotes;
         
     }
-   
+
 }
 
+// function noOfNotesToReturn(amountLeft) {
 
+//     for(i=0; i < dominationAvailable.length; i++) {
+
+//         const noofnotes = Math.trunc(amountLeft/dominationAvailable[i]);
+//         amountLeft = amountLeft % dominationAvailable[i];
+//         numberofnotes[i].innerText = noofnotes;
+        
+//     }
+   
+// }
